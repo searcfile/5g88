c336e9acdeebd000f0ff037fb4d34cd7912c1b1c
searchfile <5g88.offical@gmail.com>
2025-09-16 12:44:49 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,9 @@
-
+const firebaseConfig = {
+  apiKey: "AIzaSyDqWLQ8df-CBmOrgLEmg38ceD3dZwzuTsk",
+  authDomain: "video-98355.firebaseapp.com",
+  projectId: "video-98355",
+  storageBucket: "video-98355.firebasestorage.app",
+  messagingSenderId: "525374326586",
+  appId: "1:525374326586:web:e2578b79fad3e29df2f203"
+};
+if (!firebase.apps.length) firebase.initializeApp(firebaseConfig);
