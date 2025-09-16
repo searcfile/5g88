9f94767952a9e80785fbf278246a7f1097209c6a
searchfile <5g88.offical@gmail.com>
2025-09-16 13:13:21 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,21 @@
+// ⚠️ Ganti hanya nilai di bawah sesuai project kamu
 const firebaseConfig = {
   apiKey: "AIzaSyDqWLQ8df-CBmOrgLEmg38ceD3dZwzuTsk",
   authDomain: "video-98355.firebaseapp.com",
   projectId: "video-98355",
-  storageBucket: "video-98355.firebasestorage.app",
+  // WAJIB untuk Realtime Database:
+  databaseURL: "https://video-98355-default-rtdb.asia-southeast1.firebasedatabase.app",
+  // Storage bucket yang benar:
+  storageBucket: "video-98355.appspot.com",
   messagingSenderId: "525374326586",
   appId: "1:525374326586:web:e2578b79fad3e29df2f203"
 };
 
-// Inisialisasi sekali saja
+// Init sekali
 if (!firebase.apps || !firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 } else {
-  firebase.app(); // gunakan instance yang sudah ada
+  firebase.app();
 }
+
 console.log('Firebase apps length =', firebase.apps.length);
